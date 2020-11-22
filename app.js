39e1588547ce71995cb7fbc434e9c66791520ac6
dastratakos <dastratakos@gmail.com>
2020-11-22 14:12:05 -0800

    Tweak home page animations

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ const profile_img = document.querySelector('.profile_img')
 const tl = new TimelineMax()
 
 tl.fromTo(hero, 1, {height: "0%"}, {height: "80%", ease: Power2.easeInOut})
-.fromTo(hero, 1.2, {width: "100%"}, {width: "80%", ease: Power2.easeInOut})
+.fromTo(hero, 1.2, {width: "95%"}, {width: "80%", ease: Power2.easeInOut})
 .fromTo(slider, 1.2, {x: "-100%"}, {x: "0%", ease: Power2.easeInOut}, "=-1.2")
 .fromTo(profile_img, 0.5, {x: -20}, {x: 0, ease: Power2.easeInOut}, "=-0.5")
